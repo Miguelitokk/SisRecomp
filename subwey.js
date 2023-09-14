da1d560b7117cf5039aa4b62c0c18419a65dc150
Miguel Ferreira <miguel.ferreira@gama.local>
2023-09-14 14:19:39 -0300

    Projeto final

--- subwey.js
+++ subwey.js
@@ -12,14 +12,37 @@ const resultado = document.querySelector("#resultado");
         const dietaValor = parseInt(dietaSemana.value);
 
         if (treinoValor >= 5 && dietaValor >= 4) {
-            resultado.innerHTML = "Pode comer subway";
+            resultado.innerHTML = "Pode comer subway     ";
+            const imagemSubway = document.createElement("img");
+            imagemSubway.src = "Imgs/gatoComendoSanduiche.jpg";
+            imagemSubway.alt = "Subway";
+
+            imagemSubway.width = 200; // Defina a largura desejada em pixels
+            imagemSubway.height = 150; // Defina a altura desejada em pixels
+            
+            resultado.appendChild(imagemSubway);
         } else if (treinoValor >= 3 && dietaValor >= 2) {
-            resultado.innerHTML = "Pode comer açaí com banana";
+            resultado.innerHTML = "Pode comer açaí com banana   ";
+            const imagemAcai = document.createElement("img");
+        imagemAcai.src = "Imgs/gatoComendoAcai.jfif";
+        imagemAcai.alt = "Açaí com Banana";
+
+        imagemAcai.width = 200; // Defina a largura desejada em pixels
+        imagemAcai.height = 150; // Defina a altura desejada em pixels
+        
+        resultado.appendChild(imagemAcai);
         } else {
-            resultado.innerHTML = "Vai comer nada não, pião";
-        }
-    });
-   
+            resultado.innerHTML = "Vai comer nada não, pião   ";
+            const imagemNada = document.createElement("img");
+        imagemNada.src = "Imgs/gatoComRaiva.jfif";
+        imagemNada.alt = "Nada";
+
+        imagemNada.width = 200; // Defina a largura desejada em pixels
+        imagemNada.height = 150; // Defina a altura desejada em pixels
+
+        resultado.appendChild(imagemNada);
+    }
+});
     
     
     
